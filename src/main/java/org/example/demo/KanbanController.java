ade3338506653257c9dbc6fe55f1185b898233ea
Kevin <an.kevin3104@gmail.com>
2025-01-14 16:37:16 +0100

    Version

--- src/main/java/org/example/demo/KanbanController.java
+++ src/main/java/org/example/demo/KanbanController.java
@@ -1,7 +1,20 @@
 package org.example.demo;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+
+import java.io.IOException;
 
 public class KanbanController {
 
+    public void handleAjouterTache() {
+        try {
+            // Charger la fenêtre d'ajout de tache
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("AddTache.fxml"));
+            Parent root = loader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
