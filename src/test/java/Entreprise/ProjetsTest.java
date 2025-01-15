5c879b1c6115793094588be45dcead8ab141220d
Micky <micky.harau@gmail.com>
2025-01-15 13:51:01 +0100

    Version 31

---
+++ src/test/java/Entreprise/ProjetsTest.java
@@ -0,0 +1,84 @@
+package Entreprise;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProjetsTest {
+
+    @Test
+    void getId() {
+    }
+
+    @Test
+    void setId() {
+    }
+
+    @Test
+    void getNomDeProjet() {
+    }
+
+    @Test
+    void setNomDeProjet() {
+    }
+
+    @Test
+    void getDebut() {
+    }
+
+    @Test
+    void setDebut() {
+    }
+
+    @Test
+    void getFin() {
+    }
+
+    @Test
+    void setFin() {
+    }
+
+    @Test
+    void getListeProjets() {
+    }
+
+    @Test
+    void suprProjet() {
+    }
+
+    @Test
+    void getTitre() {
+    }
+
+    @Test
+    void getEmployes() {
+    }
+
+    @Test
+    void ajouterEmploye() {
+    }
+
+    @Test
+    void supprimerEmploye() {
+    }
+
+    @Test
+    void ajouterTache() {
+    }
+
+    @Test
+    void supprimerTache() {
+    }
+
+    @Test
+    void projetGetTache() {
+    }
+
+    @Test
+    void suprimerChercheTache() {
+    }
+
+    @Test
+    void getTaches() {
+    }
+}
---
+++ src/test/java/Entreprise/TachesTest.java
@@ -0,0 +1,64 @@
+package Entreprise;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TachesTest {
+
+    @Test
+    void modifier() {
+    }
+
+    @Test
+    void setTitre() {
+    }
+
+    @Test
+    void setPriorite() {
+    }
+
+    @Test
+    void setDateLimite() {
+    }
+
+    @Test
+    void setResponsable() {
+    }
+
+    @Test
+    void setStatut() {
+    }
+
+    @Test
+    void setCommentaires() {
+    }
+
+    @Test
+    void getId() {
+    }
+
+    @Test
+    void getTitre() {
+    }
+
+    @Test
+    void getPriorite() {
+    }
+
+    @Test
+    void getDateLimite() {
+    }
+
+    @Test
+    void getResponsable() {
+    }
+
+    @Test
+    void getStatut() {
+    }
+
+    @Test
+    void getCommentaires() {
+    }
+}
